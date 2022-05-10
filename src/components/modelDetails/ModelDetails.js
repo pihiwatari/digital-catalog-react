3e8a7bda24a4d633e1a0490ada3660d749105a69
Elias Rayas <eliasrayasg@gmail.com>
2022-05-09 22:40:25 -0500

    Added more to screen

--- src/components/modelDetails/ModelDetails.js
+++ src/components/modelDetails/ModelDetails.js
@@ -33,11 +33,27 @@ export default function ModelDetails() {
           </ul>
         </div>
       </article>
-      <aside>
-        <div>
+      <aside className="model__properties-container">
+        <div className="properties__pricing">
           <p>Estimated printing cost</p>
           <h3>$ 999.99</h3>
           <button>Download model</button>
+        </div>
+        <div className="properties__data-list-container">
+          <ul className="data-list">
+            <li className="data-list__item">
+              <h4>Category</h4>
+              <p>End-use parts</p>
+            </li>
+            <li className="data-list__item">
+              <h4>Category</h4>
+              <p>End-use parts</p>
+            </li>
+            <li className="data-list__item">
+              <h4>Category</h4>
+              <p>End-use parts</p>
+            </li>
+          </ul>
         </div>
       </aside>
     </main>
